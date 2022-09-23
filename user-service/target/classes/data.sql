a4f8919371c9234e3b04be12432d22959a6afadd
Bhanu Shukla <bhanu24shukla@gmail.com>
2022-09-23 09:32:37 +0530

    application updated

--- user-service/target/classes/data.sql
+++ user-service/target/classes/data.sql
@@ -1,2 +1,4 @@
 insert into users (name, balance) values ('sam', 1000);
 insert into users (name, balance) values ('mike', 3000);
+insert into users (name, balance) values ('ram', 2000);
+insert into users (name, balance) values ('shyam', 2500);
